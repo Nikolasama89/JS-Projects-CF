61544a1c897880f2ef41517a3b22ea3f05e664c9
Nikos <niqos89@gmail.com>
2025-02-07 20:44:54 +0200

    update

---
+++ functions/4-higher-order-functions.js
@@ -0,0 +1,14 @@
+// Higher-Order Functions
+// Function Factory
+// Closure
+function createMultiplier(multiplier) {
+  return function(num) {
+    return num * multiplier
+  }
+}
+
+const createMultiplier2 = (multiplier) => (num) => num * multiplier
+
+const double = createMultiplier(2)
+const triple = createMultiplier(3)
+console.log(double(5))
