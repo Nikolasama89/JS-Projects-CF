7537154f3358b4b0f444d7b1aac7a6bd80e30601
Nikos <niqos89@gmail.com>
2025-02-07 19:58:41 +0200

    feat: function format, optionals params

---
+++ functions/2-optional-default-params.js
@@ -0,0 +1,34 @@
+function greet(name) {
+  if (name === undefined) {
+    name = "Guest"
+  }
+  console.log(`Hello, ${name}`)
+}
+
+greet()
+
+// Default Params 
+function getUser(name = "Guest", age = 18) {
+  return { name, age }
+}
+
+const obj = getUser()
+console.log(obj);
+
+function getFormattedDateTime(
+  locale = "en-Us",
+  options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+  }
+) {
+  const now = new Date()
+  return now.toLocaleString(locale, options)
+}
+console.log(getFormattedDateTime("el-GR"))
