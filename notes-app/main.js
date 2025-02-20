ff7c28f9ef1e86b9f17427811c0b5e7575b18f3e
Nikos <niqos89@gmail.com>
2025-02-20 10:57:06 +0200

    updates

--- notes-app/main.js
+++ notes-app/main.js
@@ -6,16 +6,16 @@ let count = 0
 window.addEventListener("DOMContentLoaded", function(){
 
   function printGRDate() {
-    var today = new Date();
-    var month = today.getMonth();
-    var day = today.getDay();
-    var year = today.getFullYear();
+    let today = new Date();
+    let month = today.getMonth();
+    let day = today.getDay();
+    let year = today.getFullYear();
 
-    var hour = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
-    var minute = today.getMinutes();
-    var seconds = today.getSeconds();
+    let hour = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+    let minute = today.getMinutes();
+    let seconds = today.getSeconds();
 
-    var outputDate = `${daysGR[day]} ,  ${today.getDate()} ${monthsGR[month]} ${year}`
+    let outputDate = `${daysGR[day]} ,  ${today.getDate()} ${monthsGR[month]} ${year}`
     let outputHour = `${hour} : ${minute} : ${seconds}`
     document.getElementById("dateTxt").innerHTML = `${outputDate} <br> ${outputHour}`
     
