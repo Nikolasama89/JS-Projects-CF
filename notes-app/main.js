4122e79d3b4b60308ef91e6e5259c8a06bc6742f
Nikos <niqos89@gmail.com>
2025-02-20 10:52:16 +0200

    feat: add date and time

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,12 +1,27 @@
 const daysGR = ["Κυριακή", "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο"]
-const monthsGR = ["Ιανουαρίου", "Φεβρουαρίου", "Μαρτίου", "Απριλίου", "Μαϊου", "Ιουνίου", "Ιουλίου", "Αυγόυστου", "Σεπτεμβρίου", "Οκτωμβρίυ", "Νοεμβρίου", "Δεκεμβρίου"]
-
+const monthsGR = ["Ιανουαρίου", "Φεβρουαρίου", "Μαρτίου", "Απριλίου", "Μαίου", "Ιουνίου", "Ιουλίου", "Αυγoύστου", "Σεπτεμβρίου", "Οκτωμβρίυ", "Νοεμβρίου", "Δεκεμβρίου"]
 let notes = []
 let count = 0
 
 window.addEventListener("DOMContentLoaded", function(){
 
+  function printGRDate() {
+    var today = new Date();
+    var month = today.getMonth();
+    var day = today.getDay();
+    var year = today.getFullYear();
+
+    var hour = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+    var minute = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    var outputDate = `${daysGR[day]} ,  ${today.getDate()} ${monthsGR[month]} ${year}`
+    let outputHour = `${hour} : ${minute} : ${seconds}`
+    document.getElementById("dateTxt").innerHTML = `${outputDate} <br> ${outputHour}`
+    
+  }
   this.setInterval(printGRDate, 1000)
+  
 
 
   this.document.querySelector("#addNoteBtn").addEventListener("click", function(){
