b25e94586821c70ff5361e171145897e4df3ed32
Nikos <niqos89@gmail.com>
2025-03-12 13:17:38 +0200

    fix: comments

--- apps/movies-app/js/index.js
+++ apps/movies-app/js/index.js
@@ -12,43 +12,53 @@
 //   })
 // })
 
-// changed project from jquery to vanilla js
+// Αλλαγη απο jquery to vanilla js
+// Εκτελεση αφου φορτωσει το DOM
 document.addEventListener("DOMContentLoaded", function () {
 
   let debounceTimeout = null;
   const searchInput = document.getElementById("searchInput")
   const showMore = document.getElementById("showMore")
 
+  // Event listener για αναζητηση με debounce (αναμονη πριν εκτελεστει η κληση στο ΑΠΙ)
   searchInput.addEventListener("input", function () {
     clearTimeout(debounceTimeout)
-    debounceTimeout = setTimeout(() => getMovie(this.value.trim()), 1500)
+    debounceTimeout = setTimeout(() => getMovie(this.value.trim()), 1500) // Εκτελεση getMovie
   });
 
+  // Event listener για κουμπι "Περισσοτερα"
   showMore.addEventListener("click", function(e) {
     e.preventDefault();
     onShowMoreClicked();
   })
 });
 
+// μεταβλητη για αποθηκευση των δεδομενων της ταινιας
 let movieData = {};
 
+// function για να παρουμε δεδομενα της ταινιας απο το ΑΠΙ
 const getMovie = async (title) => {
   // const apiKey = "b7c16c10"
   try {
-    console.log("Searching for:", title)
+    console.log("Searching for:", title) // DEBBUGING
+    
+    // αποστολη ΗTTP GET στο backend server
     let response = await axios.get(`http://localhost:3000/movies?title=${title}`, {
       params : {
         t: title
       }
     });
-    console.log("API Response: ", response.data)
+    console.log("API Response: ", response.data) // DEBUGGING
+    
+    // αποθηκευση movie data
     movieData = response.data;
+    
+    // ανακτηση και ενημερωση εικονας στο DOM
     let moviePoster = response.data.Poster
-      
     let movieImg = document.getElementById("image")
     movieImg.src = moviePoster
 
-
+    // ενημερωση DOM με τις πληροφοριες
     document.getElementById("title").innerHTML = movieData.Title;
     document.getElementById("year").innerHTML = `Έτος παραγωγής: ${response.data.Year}`
     document.getElementById("runtime").innerHTML = `Διάρκεια: ${response.data.Runtime}`
@@ -63,6 +73,7 @@ const getMovie = async (title) => {
     }
   };
     
+// function για οταν πατηθει το κουμπι περισσοτερα
 const onShowMoreClicked = () => {
     document.getElementById("director").innerHTML = `Σκηνοθεσία: ${movieData.Director}`;
     document.getElementById("actors").innerHTML = `Ηθοποιοί: ${movieData.Actors}`;
@@ -72,7 +83,7 @@ const onShowMoreClicked = () => {
     document.getElementById("rated").innerHTML = `Καταλληλότητα: ${movieData.Rated}`;
 
     // document.getElementById("extraInfo").classList.remove("hidden")
-    // Toggles the extraInfo id when user press it 
+    // Toggles the extraInfo id oταν πατηθει
     let extraInfoElement = document.getElementById("extraInfo")
     extraInfoElement.classList.toggle("hidden")
 }
--- apps/movies-app/movie-backend/server.js
+++ apps/movies-app/movie-backend/server.js
@@ -1,18 +1,26 @@
+// Φορτωση μεταβλητων απο .env
 require("dotenv").config();
+
+// Εισαγωγη βιβλιοθηκών 
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
+// Δημιουργια Express εφαρμογης και ορισμος θυρας server
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// Χρηση middleware σε περιπτωση που ειχαμε διαφορετικους σερβερ το frontend και το backend
+// app.use(cors());
 
+// Route για την αναζητηση ταινίας με ΟΜDB API
 app.get("/movies", async(req, res) => {
   try {
     // const title = req.query.title;
     //destructuring assignment 
     const {title} = req.query;
+    
+    // HTTP GET request OMDB API με το API KEY τον τιτλο που πηραμε με destructuring 
     const response = await axios.get (`https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${title}`);
     res.json(response.data);
   } catch (error) {
